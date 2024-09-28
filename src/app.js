61385f598b57b5a1dcbe2e835f9ebe6a63f292bb
Kowsalya Srinivasan <kowsisrini25@gmail.com>
2024-09-28 20:50:11 +0530

    DevConnnect - Exploring routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,46 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("This is the home page");
+app.use("/test", (req, res) => {
+  res.send("Test route");
 });
 
-app.use("/test", (req, res) => {
-  res.send("Hello from server");
+// app.get("/a(bc)+d", (req, res) => {
+//   res.send("abc");
+// });
+// app.get("/a(bc)?d", (req, res) => {
+//   res.send("abc");
+// });
+// app.get("/ab*cd", (req, res) => {
+//   res.send("abc");
+// });
+//regex
+app.get(/a/, (req, res) => {
+  res.send("abc");
+});
+
+app.get(/.*fly$/, (req, res) => {
+  res.send("abc");
+});
+
+// app.get("/user", (req, res) => {
+//   console.log(req.query);
+
+//   res.send("user data: kowsalya srinivasan");
+// });
+
+app.get("/user/:id/:name", (req, res) => {
+  console.log(req.params);
+
+  res.send("user data: kowsalya srinivasan");
+});
+
+app.post("/user", (req, res) => {
+  res.send("user data saved successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("user deleted successfully");
 });
 
 app.listen(3000, () => {
