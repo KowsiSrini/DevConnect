53b7354ff60e51cab08d8331423894463a7fa450
Kowsalya Srinivasan <kowsisrini25@gmail.com>
2024-09-28 22:00:39 +0530

    DevConnect - handling multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,47 +1,18 @@
 const express = require("express");
 const app = express();
-
-app.use("/test", (req, res) => {
-  res.send("Test route");
-});
-
-// app.get("/a(bc)+d", (req, res) => {
-//   res.send("abc");
-// });
-// app.get("/a(bc)?d", (req, res) => {
-//   res.send("abc");
-// });
-// app.get("/ab*cd", (req, res) => {
-//   res.send("abc");
-// });
-//regex
-app.get(/a/, (req, res) => {
-  res.send("abc");
-});
-
-app.get(/.*fly$/, (req, res) => {
-  res.send("abc");
-});
-
-// app.get("/user", (req, res) => {
-//   console.log(req.query);
-
-//   res.send("user data: kowsalya srinivasan");
-// });
-
-app.get("/user/:id/:name", (req, res) => {
-  console.log(req.params);
-
-  res.send("user data: kowsalya srinivasan");
-});
-
-app.post("/user", (req, res) => {
-  res.send("user data saved successfully");
-});
-
-app.delete("/user", (req, res) => {
-  res.send("user deleted successfully");
-});
+// route handler can be wrapped in array
+//app.use("/user",[h1,h2,h3,h4,h5])
+// app.use("/user",[h1,h2],h3,h4,h5)
+app.use(
+  "/user",
+  (req, res, next) => {
+    res.send("user 1");
+    next();
+  },
+  (req, res) => {
+    res.send("user 2");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 4000");
