ea3833464f45b07aa4ab2d3a77cb10a679d4f7d5
Kowsalya Srinivasan <kowsisrini25@gmail.com>
2024-09-29 19:05:59 +0530

    DevConnect - Error Handling

--- src/app.js
+++ src/app.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const app = express();
-const { adminauth, userAuth } = require("./middlewares/auth");
-app.use("/admin", adminauth);
 
-app.get("/admin/getData", (req, res) => {
-  res.send("Sent all data");
-});
-
-app.delete("/admin/deletedata", (req, res) => {
-  res.send("data deleted succesfully");
-});
-
-app.get("/user", userAuth, (req, res) => {
+app.get("/user", (req, res) => {
+  throw new Error("error");
   res.send("user data sent");
 });
 
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong");
+  }
+});
 app.listen(3000, () => {
   console.log("Server is successfully listening on port 4000");
 });
