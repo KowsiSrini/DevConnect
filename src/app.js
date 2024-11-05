5c7f3adfab2839c937e7244c33ad49b9ce24aea9
Kowsalya Srinivasan <kowsisrini25@gmail.com>
2024-11-05 22:18:19 +0530

    [Init database]

--- src/app.js
+++ src/app.js
@@ -2,12 +2,27 @@ const express = require("express");
 require("./config/database");
 const app = express();
 const connectDB = require("./config/database");
+const User = require("./models/user");
+app.post("/signup", async (req, res) => {
+  const userObj = new User({
+    firstName: "Kowsalya",
+    lastName: "Srinivasan",
+    emailId: "kowsisrini25@gmail.com",
+    password: "kowsi@123",
+  });
+  try {
+    await userObj.save();
+    res.send("user added successfully");
+  } catch (err) {
+    res.status(400).send("Error saving the user ", err.message);
+  }
+});
 
 connectDB()
   .then(() => {
     console.log("DB connected successfully");
     app.listen(3000, () => {
-      console.log("Server is successfully listening on port 4000");
+      console.log("Server is successfully listening on port 3000");
     });
   })
   .catch((err) => {
