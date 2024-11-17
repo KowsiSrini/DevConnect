abb5aef773c551aa21f7f2090f74d23a895b128a
Kowsalya Srinivasan <kowsisrini25@gmail.com>
2024-11-17 18:31:39 +0530

    DevConnect - forgot password API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,13 +1,62 @@
 const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth.js");
-profileRouter.get("/profile", userAuth, async (req, res) => {
+const bcrypt = require("bcrypt");
+const {
+  validateEditProfile,
+  validatePwdField,
+} = require("../utils/validator.js");
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
-
     res.send(user);
   } catch (error) {
     res.status(400).send("Profile Error " + error.message);
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfile(req)) {
+      throw new Error("Invalid Edit request");
+    }
+    const loggedInUser = req.user;
+    console.log(req.body);
+
+    console.log({ loggedInUser });
+
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+    loggedInUser.save();
+    res.json({
+      message: `${loggedInUser.firstName}, your data updated successfully !`,
+      data: loggedInUser,
+    });
+  } catch (error) {
+    res.status(400).send("Error " + error);
+  }
+});
+
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    if (!validatePwdField(req)) {
+      throw new Error("Enter old password and new password");
+    }
+    const loggedInUser = req.user;
+    const enteredOldPwd = req.body.oldpassword;
+    const enteredNewPwd = req.body.newpassword;
+    const isoldpwdMatches = await loggedInUser.getPasswordValid(enteredOldPwd);
+    console.log({ isoldpwdMatches });
+    if (isoldpwdMatches) {
+      const newPasswordHash = await bcrypt.hash(enteredNewPwd, 10);
+      loggedInUser.password = newPasswordHash;
+      loggedInUser.save();
+    } else {
+      throw new Error("Current password is not correct");
+    }
+
+    res.send("password changed scucessfully");
+  } catch (error) {
+    res.status(400).send("Error " + error.message);
   }
 });
 module.exports = { profileRouter };
--- src/utils/validator.js
+++ src/utils/validator.js
@@ -10,4 +10,25 @@ const validateSignupData = (req) => {
   }
 };
 
-module.exports = { validateSignupData };
+const validateEditProfile = (req) => {
+  const allowedEditFields = ["age", "gender", "about", "skill", "photo"];
+
+  const isEditValid = Object.keys(req.body).every((key) =>
+    allowedEditFields.includes(key)
+  );
+
+  return isEditValid;
+};
+
+const validatePwdField = (req) => {
+  const pwdFields = ["oldpassword", "newpassword"];
+
+  let isPwdChange = true;
+
+  const missingFields = pwdFields.filter((field) => !(field in req.body));
+  if (missingFields.length > 0) {
+    isPwdChange = false;
+  }
+  return isPwdChange;
+};
+module.exports = { validateSignupData, validateEditProfile, validatePwdField };
